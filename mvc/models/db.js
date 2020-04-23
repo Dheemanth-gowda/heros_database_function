196ae520e316fef5dcaabe34b99252c50bc95d8d
Dheemanth-gowda <dheethu12@gmail.com>
2020-04-23 10:46:44 +0530

    initial commit

---
+++ mvc/models/db.js
@@ -0,0 +1,46 @@
+const Mongoose = require("mongoose");
+
+let uri = "mongodb://localhost/the_supers_prod";
+
+Mongoose.connect(uri, { useNewUrlParser: true });
+
+Mongoose.connection.on("connected", () => {
+    console.log("============================");
+    console.log("============================");
+    console.log(`Connected to the uri ${uri}`);
+    console.log("============================");
+    console.log("============================");
+});
+
+Mongoose.connection.on("error", (err) => {
+    console.log(`mongoose connection has some error ${err}`);
+});
+
+Mongoose.connection.on("diconnected", () => {
+    console.log("MOngoose disconnected");
+});
+
+const shutdowm = (msg, callback) => {
+    Mongoose.connection.close(() => {
+        console.log(`mongoose disconnected through ${msg}`);
+        callback();
+    });
+};
+
+process.once("SIGUSR2", () => {
+    shutdown("nodemon restart", () => {
+        process.kill(process.pid, "SIGUSR2");
+    });
+});
+
+process.on("SIGINT", () => {
+    shutdowm("app termination", () => {
+        process.exit(0);
+    });
+});
+
+process.on("SIGTERM", () => {
+    shutdowm("Hereko app shutdown", () => {
+        process.exit(0);
+    });
+});
